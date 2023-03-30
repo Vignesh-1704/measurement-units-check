e1dff27561363cdc926e6c9aa86b2235dbb50cdc
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-30 11:59:43 +0530

    Requirement 1 Add. Included The Logic , Test Case And the Hamcrest Dependency For Better Code Readability

--- src/main/java/com/m2p/UnitsCheck.java
+++ src/main/java/com/m2p/UnitsCheck.java
@@ -1,4 +1,7 @@
 package com.m2p;
 
 public class UnitsCheck {
+    public boolean checkEquality(String firstMeasurement, String secondMeasurement) {
+        return firstMeasurement.equals(secondMeasurement);
+    }
 }
