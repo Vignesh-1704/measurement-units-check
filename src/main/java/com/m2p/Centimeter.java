5f5c62e72fc4450c662dd3482e3d75d041b129e9
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-30 15:35:53 +0530

    Requirement 2 Add.Included the Logic And Test Cases for M to Cm , Cm to KmConversion

--- src/main/java/com/m2p/Centimeter.java
+++ src/main/java/com/m2p/Centimeter.java
@@ -10,6 +10,15 @@ public class Centimeter {
         this.magnitude = magnitude;
     }
 
+    public static Centimeter conversionMetreToCm(double meter)
+    {
+        return new Centimeter(meter * 100);
+    }
+    public static Centimeter conversionCmToKm(double cm)
+    {
+        return new Centimeter(cm / 100000 );
+    }
+
     @Override
     public boolean equals(Object object){
         if(this == object){
--- src/test/java/com/m2p/CentimeterTest.java
+++ src/test/java/com/m2p/CentimeterTest.java
@@ -2,7 +2,10 @@ package com.m2p;
 
 import org.junit.jupiter.api.Test;
 
+import static com.m2p.Centimeter.conversionCmToKm;
+import static com.m2p.Centimeter.conversionMetreToCm;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -15,6 +18,47 @@ public class CentimeterTest {
         Centimeter anotherCentimeter = new Centimeter(1);
 
         assertThat(oneCentimeter , is(equalTo(anotherCentimeter)));
+    }
+
+    @Test
+    void toReturnFalseIf1cmIsNotEqualTo1cm()
+    {
+        Centimeter oneCentimeter = new Centimeter(1);
+        Centimeter anotherCentimeter = new Centimeter(3);
+
+        assertThat(oneCentimeter , is(not(equalTo(anotherCentimeter))));
+    }
+
+    @Test
+    void toReturn100CmIf1MeterIsGiven()
+    {
+        Centimeter oneMeter = conversionMetreToCm(1);
+        Centimeter hundredCm = new Centimeter(100);
+        assertThat(oneMeter, is(equalTo(hundredCm)));
+    }
+
+    @Test
+    void toReturnNotEqual100CmIf1MeterIsGiven()
+    {
+        Centimeter oneMeter = conversionMetreToCm(1);
+        Centimeter hundredCm = new Centimeter(1000);
+        assertThat(oneMeter, is(not(equalTo(hundredCm))));
+    }
+
+
+    @Test
+    void toReturnTrue001KmWhen100cmIsGiven()
+    {
+        Centimeter kmConversion = conversionCmToKm(100);
+        Centimeter kilometer = new Centimeter(0.001);
+        assertThat(kmConversion,is(equalTo(kilometer)));
+    }
+    @Test
+    void toReturnFalse01KmWhen100cmIsGiven()
+    {
+        Centimeter kmConversion = conversionCmToKm(100);
+        Centimeter kilometer = new Centimeter(0.1);
+        assertThat(kmConversion,is(not(equalTo(kilometer))));
     }
 
 }
